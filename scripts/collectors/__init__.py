abf8690cbf7410342394ae233561bd4b67f19b72
Andrew Couch <53659320+Ghost081280@users.noreply.github.com>
2026-01-09 12:00:54 -0500

    Update __init__.py

--- scripts/collectors/__init__.py
+++ scripts/collectors/__init__.py
@@ -4,5 +4,6 @@ from .job_collector import JobCollector
 from .fec_collector import FECCollector
 from .nonprofit_collector import NonprofitCollector
 from .news_collector import NewsCollector
+from .ddg_collector import DuckDuckGoCollector
 
-__all__ = ['JobCollector', 'FECCollector', 'NonprofitCollector', 'NewsCollector']
+__all__ = ['JobCollector', 'FECCollector', 'NonprofitCollector', 'NewsCollector', 'DuckDuckGoCollector']
