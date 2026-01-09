d8258c1699191c08c7a9e0a4716f62985d5158f4
Andrew Couch <53659320+Ghost081280@users.noreply.github.com>
2026-01-09 13:59:30 -0500

    Update __init__.py

--- scripts/collectors/__init__.py
+++ scripts/collectors/__init__.py
@@ -1,9 +1,17 @@
-"""Collectors package - Data collection modules for Astroturf Detector"""
+"""Collectors package"""
 
 from .job_collector import JobCollector
 from .fec_collector import FECCollector
 from .nonprofit_collector import NonprofitCollector
 from .news_collector import NewsCollector
 from .ddg_collector import DuckDuckGoCollector
+from .campaign_finance_collector import CampaignFinanceCollector
 
-__all__ = ['JobCollector', 'FECCollector', 'NonprofitCollector', 'NewsCollector', 'DuckDuckGoCollector']
+__all__ = [
+    'JobCollector',
+    'FECCollector',
+    'NonprofitCollector',
+    'NewsCollector',
+    'DuckDuckGoCollector',
+    'CampaignFinanceCollector'
+]
